333a44291a35f57c27da1d6487ba7f1fb285b1c7
Wai Keat <waikeat@meta.us>
2020-02-09 00:13:55 +0800

    [Routers/users/utils] Change password saving from plaintext to hashed

--- app/routers/users/utils.py
+++ app/routers/users/utils.py
@@ -16,8 +16,10 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    from app.routers.auth.utils import get_password_hash
+
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
