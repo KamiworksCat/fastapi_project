d10d43f27065ff8862ba26f6cbacf72f37360133
Wai Keat <waikeat@meta.us>
2020-02-11 23:51:25 +0800

    [Routers] Add more endpoints for user api

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,8 @@
 from typing import List
 
-from fastapi import Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
 
 import crud
@@ -55,3 +57,38 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.delete("/{user_id}", response_model=schemas.User, summary="Delete specific user")
+def delete_user(user_id: int, db: Session = Depends(get_db),
+                user: models.User = Depends(get_current_active_user)):
+    if not user.is_administrator:
+        raise HTTPException(status_code=403, detail="User cannot perform this action")
+    db_user = crud.user.get(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    crud.user.remove(db_session=db, obj_id=user_id)
+    return "User has been removed from the system"
+
+
+@router.put("/{user_id}", response_model=schemas.User)
+def update_user(
+    *,
+    db: Session = Depends(get_db),
+    user_id: int,
+    user_in: schemas.UserUpdate,
+    current_user: models.User = Depends(get_current_active_user),
+):
+    """
+    Update a user.
+    """
+    user = crud.user.get(db, id=user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist in the system",
+        )
+    elif user != current_user or not current_user.is_administrator:
+        raise HTTPException(status_code=403, detail="You may only update your own profile")
+    user = crud.user.update(db, db_obj=user, obj_in=user_in)
+    return user
