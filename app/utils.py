703bd7eae31c1604358a62c1ce47e800bf3ff871
Wong Wai Keat <waikeat@meta.us>
2020-02-14 16:15:16 +0800

    [app/utils] Update token form url and get_current_user function

--- app/utils.py
+++ app/utils.py
@@ -1,21 +1,23 @@
 from datetime import timedelta, datetime
 
 import jwt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
 from jwt import PyJWTError
 from passlib.context import CryptContext
-from starlette.status import HTTP_401_UNAUTHORIZED
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_403_FORBIDDEN
 
-from app import crud
+from app import crud, config
 from app.config import SECRET_KEY, ALGORITHM
 from app.database import db_session
 from app.schema.auth import TokenData
-from app.schema.user import User
+from app.models.user import User
+from app.database import get_db
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{config.API_PREFIX}/login/token")
 
 
 def verify_password(plain_password, hashed_password):
@@ -27,7 +29,7 @@ def get_password_hash(password):
 
 
 def authenticate_user(db: db_session, email: str, password: str):
-    user = crud.user.get_by_email(db, email)
+    user = crud.user.get_by_email(db_session=db, email=email)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
@@ -46,23 +48,15 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+async def get_current_user(db: Session = Depends(get_db), token: str = Security(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = TokenData(**payload)
     except PyJWTError:
-        raise credentials_exception
-    user = crud.user.get_by_email(db_session, email=token_data.email)
-    if user is None:
-        raise credentials_exception
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials")
+    user = crud.user.get(db, obj_id=token_data.user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
